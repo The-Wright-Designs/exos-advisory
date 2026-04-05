e75f2d3bbc261c86f0c258b97d8dd93703cb1cf6
Chad Wright <justdeejays@gmail.com>
2026-04-05 20:12:54 +0200

    feat: add "the pathfinder playbook section" to about page

--- _components/about-page/how-it-works/steps/how-it-works-step-two.tsx
+++ _components/about-page/how-it-works/steps/how-it-works-step-two.tsx
@@ -1,4 +1,5 @@
-import EvolvingEntrepreteurComponent from "./evolving-entrepreneur-component";
+import EvolvingEntrepreteurComponent from "./step-two/evolving-entrepreneur-component";
+import ThePathfinderPlaybookComponent from "./step-two/the-pathfinder-playbook-component";
 
 export default function HowItWorksStepTwo() {
   return (
@@ -48,6 +49,7 @@ export default function HowItWorksStepTwo() {
           </div>
         </div>
       </div>
+      <ThePathfinderPlaybookComponent />
     </div>
   );
 }
